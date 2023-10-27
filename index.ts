647cdbbe21da60843ff1c4a79bb6a6ecf64da0e0
EdamAme-x <edame8080@gmail.com>
2023-10-27 09:58:39 +0900

    add typescript

---
+++ index.ts
@@ -0,0 +1,29 @@
+import initSwc, { Options, Output, transformSync } from '@swc/wasm-web';
+
+export class JSXCompiler {
+    config: Options;
+
+    constructor(config?: Options) {
+        this.config = config ? config :  {
+            jsc: {
+              parser: {
+                syntax: 'typescript',
+                tsx: true,
+              },
+              transform: {
+                react: {
+                  pragma: 'jsx',
+                  pragmaFrag: 'Fragment',
+                  throwIfNamespace: true,
+                  development: false,
+                  useBuiltins: false,
+                },
+              },
+            },
+          };
+    }
+
+    async compile(code: string): Promise<Output> {
+        return transformSync(code, this.config)
+    }
+}
